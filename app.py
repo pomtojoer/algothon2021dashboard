b7037a2ea4797da555e539084ac0232d8e9953ac
tommrazek <tommrazek.isc@gmail.com>
2021-03-26 20:17:57 +0000

    Added plotly

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import json
+import plotly
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	graphs = [dict(data=[dict(x=[1, 2, 3], y=[10, 20, 30],   type='scatter'  ), ], layout=dict(  title='first graph' ))]
+	ids = ['graph-{}'.format(i) for i, _ in enumerate(graphs)]
+	graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
+	return render_template('index.html',ids=ids,graphJSON=graphJSON)
 
 if __name__ == '__main__':
     app.run()
